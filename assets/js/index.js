b44cea601c735080adc83d8901f1ff505bcb703a
sunnylost <sunnylost@gmail.com>
2015-01-27 15:57:29 +0800

    update styles, fetch html content

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,12 @@
     setTimeout( () => {
         $( '#overlay' )[ 0 ].classList.add( 'hide' )
     }, 1000 )
+
+    global.fetch( 'contents/indeterminate/2015-01-25.html' )
+        .then( resp =>
+            resp.text()
+        )
+        .then( html =>
+            $( '#article' )[ 0 ].innerHTML = html
+        )
 }( window )
