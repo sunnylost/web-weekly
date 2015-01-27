8139678001035fe492079cdf379ffd0dfe49b545
sunnylost <sunnylost@gmail.com>
2015-01-27 12:23:36 +0800

    automate email generating :sparkles:

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,35 @@
-var gulp       = require( 'gulp' ),
+var fs         = require( 'fs' ),
+    gulp       = require( 'gulp' ),
     to5        = require( 'gulp-6to5' ),
     concat     = require( 'gulp-concat' ),
-    stylus     = require( 'gulp-stylus' ),
-    sourcemaps = require( 'gulp-sourcemaps' ),
+    data       = require( 'gulp-data' ),
+    jade       = require( 'gulp-jade' ),
+    juice      = require( 'gulp-juice' ),
     livereload = require( 'gulp-livereload' ),
-    gutil      = require( 'gulp-util' ),
-    markdown   = require( 'gulp-markdown-to-json' ),
+    markdown   = require( 'gulp-markdown' ),
+    rename     = require( 'gulp-rename' ),
+    sourcemaps = require( 'gulp-sourcemaps' ),
+    stylus     = require( 'gulp-stylus' ),
+    dateExtend = require( 'date-extended' ),
+
+    date   = new Date( 2015, 0, 25 ),// need manual edit
+    mdName = dateExtend.format( date, 'yyyy-MM-dd' ),
 
     paths = {
         src: {
             scripts: 'assets/js/*.js',
-            styles : 'assets/styles/*.styl'
+            styles : 'assets/styles/*.styl',
+            markdown: 'contents/source/' + mdName + '.md'
+        },
+
+        indeterminate: {
+            markdown: 'contents/indeterminate/'
         },
 
         build: {
             scripts: 'build/js/',
-            styles : 'build/css/'
+            styles : 'build/css/',
+            email: 'contents/email/'
         }
     }
 
@@ -43,6 +57,30 @@ gulp.task( 'watch', function() {
     gulp.watch( paths.src.scripts, [ 'scripts' ] )
     gulp.watch( paths.src.styles, [ 'stylus' ] )
     gulp.watch( 'index.html', [ 'default' ] )
+})
+
+gulp.task( 'convert', function() {
+    gulp.src( paths.src.markdown )
+        .pipe( markdown() )
+        .pipe( gulp.dest( paths.indeterminate.markdown ) )
+})
+
+gulp.task( 'email', function() {
+    var mdFile = mdName + '.html'
+
+    gulp.src( 'assets/tmpl/email.jade' )
+        .pipe( data(function() {
+            return {
+                pageTitle: 'Web Weekly',
+                content: String( fs.readFileSync( paths.indeterminate.markdown + mdFile ))
+            }
+        }))
+        .pipe( rename( mdFile ) )
+        .pipe( jade({
+            pretty: true
+        }) )
+        .pipe( juice() )
+        .pipe( gulp.dest( paths.build.email ) )
 })
 
 gulp.task( 'default', [ 'watch', 'scripts', 'stylus' ] )
