8f4aea97512cf6a58c8da1ddb9038c828e05f86b
sunnylost <sunnylost@gmail.com>
2015-01-31 19:59:25 +0800

    rebuild

--- build/js/index.js
+++ build/js/index.js
@@ -1,36 +1,35 @@
 "use strict";
 
 ~(function (global) {
-  "use strict";
+    "use strict";
 
-  var doc = global.document,
-      base = undefined;
+    var doc = global.document,
+        base = undefined;
 
-  base = {
-    $: function $(selector) {
-      return doc.querySelectorAll(selector);
+    base = {
+        $: function $(selector) {
+            return doc.querySelectorAll(selector);
+        }
+    };
+
+    for (var key in base) {
+        global[key] = base[key];
     }
-  };
-
-  for (var key in base) {
-    global[key] = base[key];
-  }
 })(window);
 "use strict";
 
 ~(function (global) {
-  "use strict";
+    "use strict";
 
-  var $ = global.$,
-      fetch = global.fetch;
+    var $ = global.$,
+        fetch = global.fetch;
 
-  fetch("contents/indeterminate/2015-01-25.html").then(function (resp) {
-    return resp.text();
-  }).then(function (html) {
-    $("#article")[0].innerHTML = html;
-    setTimeout(function () {
-      return $("#overlay")[0].classList.add("hide");
-    }, 1000);
-  });
+    fetch("contents/indeterminate/2015-01-25.html").then(function (resp) {
+        return resp.text();
+    }).then(function (html) {
+        $("#article")[0].innerHTML = html;
+        setTimeout(function () {
+            return $("html")[0].classList.add("loaded");
+        }, 1000);
+    });
 })(window);
-//# sourceMappingURL=index.js.map
