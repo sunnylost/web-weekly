326296371d8b425d21ec62b275de965f88b08c7c
sunnylost <sunnylost@gmail.com>
2015-02-02 16:42:51 +0800

    rebuild

--- build/js/index.js
+++ build/js/index.js
@@ -22,14 +22,39 @@
     "use strict";
 
     var $ = global.$,
-        fetch = global.fetch;
+        fetch = global.fetch,
+        hash = location.hash,
+        rdate = /^#\d{4}\-\d{2}\-\d{2}$/;
 
-    fetch("contents/indeterminate/2015-01-25.html").then(function (resp) {
+    fetch("datas.json").then(function (resp) {
+        return resp.json();
+    }).then(function (data) {
+        var match = undefined,
+            issue = undefined;
+
+        if (match = hash.match(rdate)) {
+            match = match[0];
+
+            issue = data.filter(function (date) {
+                return match == date;
+            });
+
+            issue = issue.length ? issue[0] : data.pop();
+        } else {
+            issue = data.pop();
+        }
+
+        return fetch("contents/indeterminate/" + issue + ".html");
+    }).then(function (resp) {
         return resp.text();
     }).then(function (html) {
         $("#article")[0].innerHTML = html;
         setTimeout(function () {
             return $("html")[0].classList.add("loaded");
         }, 1000);
+    })["catch"](function (err) {
+        return console.log(err);
     });
+
+    "You came into a deserted place.";
 })(window);
