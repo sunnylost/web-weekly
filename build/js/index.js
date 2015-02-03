32df46b97c22713f6edf2ee36ed8f38b1f2d9f1c
sunnylost <sunnylost@gmail.com>
2015-02-03 15:44:20 +0800

    rebuild

--- build/js/index.js
+++ build/js/index.js
@@ -23,36 +23,55 @@
 
     var $ = global.$,
         fetch = global.fetch,
-        hash = location.hash,
-        rdate = /^#\d{4}\-\d{2}\-\d{2}$/;
+        htmlEl = $("html")[0],
+        articleEl = $("#article")[0],
+        rdate = /#(\d{4}\-\d{2}\-\d{2})/,
+        LOAD_CLASS = "loaded",
+        listData = undefined;
 
     fetch("datas.json").then(function (resp) {
         return resp.json();
     }).then(function (data) {
+        listData = data;
+        navigate(location.hash);
+    });
+
+    function filterName(data, name) {
+        return data.filter(function (date) {
+            return name == date;
+        });
+    }
+
+    function navigate(hash) {
         var match = undefined,
-            issue = undefined;
+            issue = undefined,
+            last = listData[listData.length - 1];
 
         if (match = hash.match(rdate)) {
-            match = match[0];
+            match = match[1];
 
-            issue = data.filter(function (date) {
-                return match == date;
-            });
+            issue = filterName(listData, match);
 
-            issue = issue.length ? issue[0] : data.pop();
-        } else {
-            issue = data.pop();
+            issue = issue.length ? issue[0] : last;
+        } else if (hash == "entries") {} else {
+            issue = last;
         }
 
-        return fetch("contents/indeterminate/" + issue + ".html");
-    }).then(function (resp) {
-        return resp.text();
-    }).then(function (html) {
-        $("#article")[0].innerHTML = html;
-        setTimeout(function () {
-            return $("html")[0].classList.add("loaded");
-        }, 1000);
-    })["catch"](function (err) {
-        return console.log(err);
-    });
+        htmlEl.classList.remove(LOAD_CLASS);
+        fetch("contents/indeterminate/" + issue + ".html").then(function (resp) {
+            return resp.text();
+        }).then(function (html) {
+            articleEl.innerHTML = html;
+            setTimeout(function () {
+                return htmlEl.classList.add(LOAD_CLASS);
+            }, 1000);
+        })["catch"](function (err) {
+            return console.log(err);
+        });
+    }
+
+    global.onhashchange = function (e) {
+        navigate(e.newURL);
+    };
 })(window);
+//TODO
